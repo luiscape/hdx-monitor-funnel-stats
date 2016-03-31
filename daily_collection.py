74ea5c2ea1bc5cd9646b5255d27f4846e707e5b7
Luis Capelo <luiscape@gmail.com>
2016-03-31 13:20:40 -0400

    Refactoring.

--- daily_collection.py
+++ daily_collection.py
@@ -1,43 +1,43 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys
-sys.path.append('./scripts')
+import json
 
 from datetime import date, timedelta
-from hdx_format import item
-from ga_collect import ga_collect
-from store_records import storeRecords
-import ckan_num_reg_users as CKANUsers
-import ckan_num_reg_orgs as CKANOrgs
+from scripts.ga_collect import ga_collect
+from scripts.utilities.prompt_format import item
+from scripts.utilities.store_records import StoreRecords
+
+import scripts.ckan_collect.ckan_num_reg_users as CKANUsers
+import scripts.ckan_collect.ckan_num_reg_orgs as CKANOrgs
 
 
 # This should be called, not defined here.
-def CollectGA(verbose = True):
-  '''Collecting the latest Google Analytics data.'''
-  print "%s Collecting Google Analytics data." % item('prompt_bullet')
-  try: 
-    records = ga_collect.get_last_weeks_data()
-    print json.dumps(records)
-    storeRecords(data = records, table = "funnel")
-    print "%s Google Analytics collection ran successfully." % item('prompt_success')
-    return True
+def collect_ga(verbose = True):
+    '''Collecting the latest Google Analytics data.'''
+    print "%s Collecting Google Analytics data." % item('prompt_bullet')
+    try:
+        records = ga_collect.get_last_weeks_data()
+        # print json.dumps(records)
+        StoreRecords(data = records, table = "funnel")
+        print "%s Google Analytics collection ran successfully." % item('prompt_success')
+        return True
 
-  except Exception as e:
-    if verbose:
-      print e
-      return False
+    except Exception as e:
+        if verbose:
+            print e
+            return False
 
     print "%s Google Analytics failed to run." % item('prompt_error')
 
 
-def Main():
+def main():
   '''Wrapper.'''
-  CollectGA()
+  collect_ga()
   CKANUsers.CollectDaily()
   CKANOrgs.CollectDaily()
 
 
 if __name__ == "__main__":
   print "%s Running daily jobs." % item('prompt_bullet')
-  Main()
+  main()

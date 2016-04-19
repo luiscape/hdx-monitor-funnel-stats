3b1aeec16d78b1f3bb932b95f3dc95e49bae2519
Luis Capelo <luiscape@gmail.com>
2016-04-19 10:53:34 -0400

    Adding week-data patcher.

---
+++ patch_week.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+PATCH
+-----
+
+Patches data for the funnel stats application.
+This is a horrendous hack: it simply deletes data
+for the current week on a schedule (hourly).
+
+    actually, I had a viewpoint: I was waiting for
+    something extraordinary to
+    happen
+
+    -- Bukowski
+
+'''
+
+import datetime
+import scraperwiki as s
+
+
+def patch(week):
+    '''
+    Deletes entries from a target week.
+    '''
+    s.sql.execute('DELETE FROM funnel WHERE period="{week}"'.format(week=week))
+    s.sql.execute('DELETE FROM funnel WHERE period="{week}"'.format(week=week))
+    print 'Entries deleted for week: %s' % week
+
+if __name__ == '__main__':
+    d = datetime.datetime.now()
+    patch(d.strftime('%Y-W%W'))
